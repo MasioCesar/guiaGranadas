1b13d86bc55bc1c8d9b7dbc7f68fe11512c0ef39
MagirusDeutz <masiomc@gmail.com>
2022-08-01 23:25:18 -0300

    adding gifs maps

--- src/components/GrenadePlace.tsx
+++ src/components/GrenadePlace.tsx
@@ -20,7 +20,7 @@ export function GrenadePlace({ gifSrc, hidden, setHidden }: GrenadePlaceProps) {
             <button>x</button>
           </div>
  
-          <picture><img className="rounded-lg pb-10" src={gifSrc} alt="" /> </picture>
+          <picture><img className="rounded-lg pb-10" src={gifSrc} alt="Gif Granada" /> </picture>
 
         </div>
 
