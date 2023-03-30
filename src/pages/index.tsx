819436e5b993e52acfce31abd0fb842df32f7e73
MasioCesar <masiomc@gmail.com>
2023-03-30 00:27:22 -0300

    adjusting width

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -23,7 +23,7 @@ export default function Login() {
             {({ errors, touched }) => (
                 <Form className="p-6 w-full flex flex-col items-center justify-center h-[90vh]">
                     <div className='p-6 flex flex-col items-center justify-center border border-[#c4ab08]'>
-                    <img src="/images/logoprataGlobal.png" alt="Logo Knife" className="mx-auto mb-4 mt-2" width="250" />
+                    <img src="/images/logoprataGlobal.png" alt="Logo Knife" className="mx-auto mb-4 mt-2" width="400" />
                     <Field name="username" className="border border-[#c4ab08] p-2 mb-4 w-full flex items-center justify-center text-black" />
                     {errors.username && touched.username ? (
                         <div className='text-[#c4ab08] mb-4'>{errors.username}</div>
@@ -32,7 +32,7 @@ export default function Login() {
                     {errors.password && touched.password ? (
                         <div className='text-[#c4ab08] mb-4'>{errors.password}</div>
                     ) : null}
-                    <button type="submit" className="border-[#c4ab08] border text-[#c4ab08] py-2 px-4 rounded mx-auto block hover:bg-yellow-400 hover:text-black w-1/4">Login</button>
+                    <button type="submit" className="border-[#c4ab08] border text-[#c4ab08] py-2 px-4 rounded mx-auto block hover:bg-yellow-400 hover:text-black w-2/4">Login</button>
                     </div>
                 </Form>
             )}
